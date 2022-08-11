be9e003076113c079429a62554576c6e070d21ae
xx1level1xx <53699465+xx1level1xx@users.noreply.github.com>
2022-08-11 15:08:03 -0700

    Create V.cpp

---
+++ V.cpp
@@ -0,0 +1,10 @@
+//sameLevelArithmeticRelationshipBetweenNodes[numLevel][i]=0;
+					//allTreePtrVar[i][col]->
+					//node temp = new node();
+					//temp.type = 
+					//allTreePtr[numLevel][prevNumNextLevel + count2].prev.next = temp;
+					//allTreePtr[numLevel][prevNumNextLevel + count2].prev = temp;
+					//for (int k = 0; k < numNextLevelConnections[numLevel - 1][j]; k++){
+					//	allTreePtr[numLevel][prevNumNextLevel + count2 + k].prev.next = temp;
+					//	allTreePtr[numLevel][prevNumNextLevel + count2 + k].prev = temp;
+					//}
