ab3b7d65a4ad01358c72ee5b2e5f4b2fb602ada2
xx1level1xx <53699465+xx1level1xx@users.noreply.github.com>
2022-08-11 18:55:44 -0700

    Update V.cpp

--- V.cpp
+++ V.cpp
@@ -1,10 +1,10 @@
 //sameLevelArithmeticRelationshipBetweenNodes[numLevel][i]=0;
-					//allTreePtrVar[i][col]->
-					//node temp = new node();
-					//temp.type = 
-					//allTreePtr[numLevel][prevNumNextLevel + count2].prev.next = temp;
-					//allTreePtr[numLevel][prevNumNextLevel + count2].prev = temp;
-					//for (int k = 0; k < numNextLevelConnections[numLevel - 1][j]; k++){
-					//	allTreePtr[numLevel][prevNumNextLevel + count2 + k].prev.next = temp;
-					//	allTreePtr[numLevel][prevNumNextLevel + count2 + k].prev = temp;
-					//}
+//allTreePtrVar[i][col]->
+//node temp = new node();
+//temp.type = 
+//allTreePtr[numLevel][prevNumNextLevel].prev.prev.next = temp;
+//allTreePtr[numLevel][prevNumNextLevel].prev.prev = temp;
+//for (int k = 0; k < numNextLevelConnections[numLevel - 1][j]; k++){
+//	allTreePtr[numLevel][prevNumNextLevel + k].prev.next = temp;
+//	allTreePtr[numLevel][prevNumNextLevel + k].prev = temp;
+//}
